ad337e7df72d12b384256082862b5960ad54d30e
Mary <manekrasova85@gmail.com>
2018-06-11 22:32:40 +0300

    Resolved module 1 task 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,20 @@
  * @returns {number}
  */
 function pow (m, n) {
+    let result = m;
 
+    for (let i = 1; i < n; i++) {
+        result *= m;
+    }
+
+    return result;
+}
+
+let m = prompt("Введите число.", '');
+let n = prompt("Введите степень.", '');
+
+if (n <= 1) {
+    alert('Введите целую степень, больше 1');
+} else {
+    alert( pow(m, n) );
 }
