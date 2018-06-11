5d88c8c2715af07214f1225decb494a738ef2c35
Mary <manekrasova85@gmail.com>
2018-06-11 22:35:34 +0300

    Resolved module 3 task 1

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,7 +4,18 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
+let age = prompt("Введите возраст");
 function showSalary(data, age) {
+    for(let obj of data){
+        if (obj.age <= +age) {
+           let name = obj.name;
+           let price = obj.balance;
+           let salary = [];
+           salary.push(name, price);
+           console.log(salary);
+
+        }
+    }
+
 }
-
-
+showSalary();
