89ec637bd4a0b20b882fe75f6b3a05c87285f9d4
Mary <manekrasova85@gmail.com>
2018-06-11 22:33:22 +0300

    Resolved module 1 task 3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -6,6 +6,28 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+let min = numbers[0];
+let max = numbers[0];
+for(let i=0; i<numbers.length; i++){
+    let number = numbers[i];
+    if (number <= min){
+        min = number;
+    }
+    if(number >= max){
+        max = number;
+    }
+}
+return{
+    min: min,
+    max: max
+}
+}
+function getMinMax(str) {
+    const arr = str.split(" ");
+    const numbers = extractNumbers(arr);
+    const minMax = extractMinMax(numbers);
+    console.log(minMax);
+    return minMax;
 
 }
-
+getMinMax(inputData);
