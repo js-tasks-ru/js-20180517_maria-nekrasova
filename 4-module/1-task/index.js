27ea443d6fc73d915486642ed41ef2ddbf4ec39c
Mary <manekrasova85@gmail.com>
2018-06-11 22:29:38 +0300

    Resolved module 4 task 1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -6,4 +6,13 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList (friends) {
+    const ul = document.createElement("ul");
+    for(let i=0; i<friends.length; i++){
+        const current = friends[i];
+        const li = document.createElement("li");
+        // li.innerText = current.firstName + " " + current.lastName;
+        li.innerText = `${current.firstName} ${current.lastName}`;
+        ul.appendChild(li);
+    }
+    return ul;
 }
